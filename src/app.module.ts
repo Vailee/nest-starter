e9419efa9c6ebb963091ac7a7ac522a8ec8c11be
gssheng <vailee@126.com>
2025-12-11 23:11:10 +0800

    feat: 集成日志模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from './common/config/config.module';
+import { LoggerModule } from './common/logger/logger.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, LoggerModule],
   controllers: [AppController],
   providers: [],
 })
---
+++ src/common/logger/createRoteteTransport.ts
@@ -0,0 +1,29 @@
+import * as DailyRotateFile from 'winston-daily-rotate-file';
+import * as winston from 'winston';
+import { Console } from 'winston/lib/winston/transports';
+import { utilities } from 'nest-winston';
+
+export const consoleTransports = new Console({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.ms(),
+    utilities.format.nestLike('gssheng'),
+  ),
+});
+
+export function createDailyRotateTransport(level: string, fileName: string) {
+  return new DailyRotateFile({
+    level,
+    dirname: 'logs',
+    filename: `${fileName}-DATE-%DATE%.log`,
+    datePattern: 'YYYY-MM-DD',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d',
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.simple(),
+    ),
+  });
+}
