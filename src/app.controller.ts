da40a1b211310a0ab41d511d8db1667c61ee3c07
gssheng <vailee@126.com>
2025-12-15 23:36:01 +0800

    feat: 使用ioredis集成redis模块

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,32 @@
-import { Controller, Get, Version } from '@nestjs/common';
+import { Controller, Get, Query, Version } from '@nestjs/common';
+// import { User } from './user/entities/user.entity';
+import { InjectRedis } from '@nestjs-modules/ioredis';
+import { Redis } from 'ioredis';
 
 @Controller()
 export class AppController {
-  constructor() {}
+  constructor(
+    // @InjectModel('User') private userModel: Model<User>,
+    @InjectRedis() private readonly redis: Redis,
+  ) {}
 
   @Get()
   getHello(): string {
     return 'Hello World!';
   }
   @Get()
-  @Version('2')
+  @Version('1')
   getHelloV2(): string {
-    return 'Hello World! v2';
+    return 'Hello World! v1';
+  }
+  @Get()
+  @Version('2')
+  async testRedis(
+    @Query('token') token: string,
+  ): Promise<Record<string, string>> {
+    console.log('🚀 ~ AppController ~ testRedis ~ token:', token);
+    const res = await this.redis.get(token);
+    await this.redis.set('token', token || '123456', 'EX', 60 * 10); // 10分钟过期
+    return { token: res };
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,34 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from './common/config/config.module';
 import { LoggerModule } from './common/logger/logger.module';
+import { RedisModule } from '@nestjs-modules/ioredis';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule, LoggerModule],
+  imports: [
+    ConfigModule,
+    LoggerModule,
+    // RedisModule.forRoot({
+    //   type: 'single',
+    //   url: 'redis://175.178.88.89:6379',
+    //   options: {
+    //     password: '123456',
+    //   },
+    // }),
+    RedisModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        console.log('🚀 ~ configService:', configService.get('REDIS_URL'));
+        return {
+          type: 'single',
+          url: 'redis://175.178.88.89:6379',
+          options: {
+            password: '123456',
+          },
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [],
 })
