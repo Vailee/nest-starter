6e830389f9bdc7e707d342132ecd9180dac3aa3c
gssheng <vailee@126.com>
2025-12-16 22:38:56 +0800

    feat: @nestjs/cache-manager & cache-manager做持久化

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Get, Query, Version } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  UseInterceptors,
+  Version,
+} from '@nestjs/common';
 // import { User } from './user/entities/user.entity';
-import { InjectRedis } from '@nestjs-modules/ioredis';
-import { Redis } from 'ioredis';
+// import { InjectRedis } from '@nestjs-modules/ioredis';
+// import { Redis } from 'ioredis';
+import { CacheInterceptor } from '@nestjs/cache-manager';
 
+let a = 0;
 @Controller()
+@UseInterceptors(CacheInterceptor) // 开启接口缓存
 export class AppController {
-  constructor(
-    // @InjectModel('User') private userModel: Model<User>,
-    @InjectRedis() private readonly redis: Redis,
-  ) {}
+  // @InjectModel('User') private userModel: Model<User>,
+  // @InjectRedis() private readonly redis: Redis,
+  constructor() {}
 
   @Get()
   getHello(): string {
@@ -25,8 +33,9 @@ export class AppController {
     @Query('token') token: string,
   ): Promise<Record<string, string>> {
     console.log('🚀 ~ AppController ~ testRedis ~ token:', token);
-    const res = await this.redis.get(token);
-    await this.redis.set('token', token || '123456', 'EX', 60 * 10); // 10分钟过期
-    return { token: res };
+    // const res = await this.redis.get(token);
+    // await this.redis.set('token', token || '123456', 'EX', 60 * 10); // 10分钟过期
+    a = a + 1;
+    return { token: a.toString() };
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from './common/config/config.module';
 import { LoggerModule } from './common/logger/logger.module';
-import { RedisModule } from '@nestjs-modules/ioredis';
-import { ConfigService } from '@nestjs/config';
-
+// import { RedisModule } from '@nestjs-modules/ioredis';
+// import { ConfigService } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
+// import { redisStore } from 'cache-manager-ioredis-yet';
 @Module({
   imports: [
     ConfigModule,
     LoggerModule,
+    //  way 1
     // RedisModule.forRoot({
     //   type: 'single',
     //   url: 'redis://175.178.88.89:6379',
@@ -16,18 +18,27 @@ import { ConfigService } from '@nestjs/config';
     //     password: '123456',
     //   },
     // }),
-    RedisModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => {
-        console.log('🚀 ~ configService:', configService.get('REDIS_URL'));
-        return {
-          type: 'single',
-          url: 'redis://175.178.88.89:6379',
-          options: {
-            password: '123456',
-          },
-        };
-      },
+    // way 2
+    // RedisModule.forRootAsync({
+    //   inject: [ConfigService],
+    //   useFactory: (configService: ConfigService) => {
+    //     console.log('🚀 ~ configService:', configService.get('REDIS_URL'));
+    //     return {
+    //       type: 'single',
+    //       url: 'redis://175.178.88.89:6379',
+    //       options: {
+    //         password: '123456',
+    //       },
+    //     };
+    //   },
+    // }),
+    // CacheModule.register
+    CacheModule.register({
+      ttl: 20000,
+      // store: redisStore,
+      host: '175.178.88.89',
+      port: 6379,
+      password: '123456',
     }),
   ],
   controllers: [AppController],
