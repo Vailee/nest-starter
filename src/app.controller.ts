8723c68b1503a6e49314983f56b840a89381cabb
gssheng <vailee@126.com>
2025-12-16 22:45:25 +0800

    feat: @nestjs/cache-manager & cache-manager做持久化- 完善

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,14 @@
-import {
-  Controller,
-  Get,
-  Query,
-  UseInterceptors,
-  Version,
-} from '@nestjs/common';
+import { Cache, CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Controller, Get, Inject, Query, Version } from '@nestjs/common';
 // import { User } from './user/entities/user.entity';
 // import { InjectRedis } from '@nestjs-modules/ioredis';
 // import { Redis } from 'ioredis';
-import { CacheInterceptor } from '@nestjs/cache-manager';
 
-let a = 0;
 @Controller()
-@UseInterceptors(CacheInterceptor) // 开启接口缓存
 export class AppController {
   // @InjectModel('User') private userModel: Model<User>,
   // @InjectRedis() private readonly redis: Redis,
-  constructor() {}
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
 
   @Get()
   getHello(): string {
@@ -29,13 +21,14 @@ export class AppController {
   }
   @Get()
   @Version('2')
-  async testRedis(
-    @Query('token') token: string,
-  ): Promise<Record<string, string>> {
+  async testRedis(@Query('token') token: string): Promise<any> {
     console.log('🚀 ~ AppController ~ testRedis ~ token:', token);
     // const res = await this.redis.get(token);
     // await this.redis.set('token', token || '123456', 'EX', 60 * 10); // 10分钟过期
-    a = a + 1;
-    return { token: a.toString() };
+
+    await this.cacheManager.set('token', token || '123456', 60 * 10); // 10分钟过期
+    const res = await this.cacheManager.get(token);
+    console.log('🚀 ~ AppController ~ testRedis ~ res:', res);
+    return { token: res };
   }
 }
